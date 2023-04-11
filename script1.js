b802be0ea7cf3732b6d28209e0ad54dbcd028366
No-Name-Nick <126535469+No-Name-Nick@users.noreply.github.com>
2023-04-11 16:39:13 +0800

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,23 @@
+function GetPass(){
+    UserName = document.getElementById("Input_1").value;
+    PassWord = document.getElementById("Input_2").value;
+    if(UserName == ""){
+        if(PassWord == ""){
+            alert("Please enter password and username");
+        }
+        else{
+            alert("Please enter username");
+        }
+    }else{
+        if(UserName == "NoNameNick"&& PassWord == "PROJQ3"){
+            window.location.href = "/Website/Websites/index.html";
+        }else if (PassWord == ""){
+            alert("Please enter password");
+        }
+        else{
+            alert("Incorrect username or password");
+        }
+    }
+//thx Franz
+//cael didnt help
+}
